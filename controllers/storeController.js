3a877edc5f2a4dd731f62d546001f630745ad481
dogukanayd <daydogdu93@gmail.com>
2018-08-17 16:04:57 +0300

    Database Storage

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,15 +1,14 @@
-exports.myMiddleware = (req, res, next) => {
-    req.name = 'dogu';
-    if(req.name === 'dogu'){
-        throw Error('that is a beatiful name');
-    }
-    res.cookie('name', 'dogu is cool', {maxAge: 90000});
-    next();
-};
-
 exports.homePage = (req, res) => {
     console.log(req.name);
     res.render('index');
+};
+
+exports.addStore = (req, res) => {
+    res.render('editStore', {title: 'Add Store'});
+};
+
+exports.createStore = (req, res) => {
+    res.json(req.body);
 };
 
  
