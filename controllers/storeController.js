e217aa6e23569e5dee2f08934fbcc65da89b9bbb
dogukanayd <daydogdu93@gmail.com>
2018-08-30 02:15:28 +0300

    Custom Queries and Hooks

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -94,3 +94,13 @@ exports.getStoreBySlug = async (req, res, next) => {
     if (!store) return next();
     res.render("store", { store, title: store.name });
 };
+
+exports.getStoresByTag = async (req, res) => {
+    const tag = req.params.tag;
+    const tagQuery = tag || { $exists: true };
+    const tagsPromise = Store.getTagsList();
+    const storePromise = Store.find({ tags: tagQuery });
+    const [tags, stores] = await Promise.all([tagsPromise, storePromise]);
+
+    res.render("tag", { tags, title: "Tags", tag, stores });
+};
