ee27b27bdf321bfef51416940ad125950f646e32
dogukanayd <daydogdu93@gmail.com>
2018-09-20 10:40:10 +0300

    Ajax REST API 2

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -1,10 +1,12 @@
 import "../sass/style.scss";
 
 import { $, $$ } from "./modules/bling";
-
 import autocomplete from "./modules/autocomplete.js";
 import typeAhead from "./modules/typeAhead.js";
+import makeMap from "./modules/map";
 
 autocomplete($("#address"), $("#lat"), $("#lng"));
 
 typeAhead($(".search"));
+
+makeMap($("#map"));
