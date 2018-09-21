ce0fbd8cc42e55905e2f9b4e9243b4db12d13f81
dogukanayd <daydogdu93@gmail.com>
2018-09-21 12:22:42 +0300

    Advanced Relationships Aggregations

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -6,177 +6,179 @@ const jimp = require("jimp");
 const uuid = require("uuid");
 
 const multerOptions = {
-    storage: multer.memoryStorage(),
-    fileFilter(req, file, next) {
-        const isPhoto = file.mimetype.startsWith("image/");
-        if (isPhoto) {
-            next(null, true);
-        } else {
-            next({ message: "That file type isn't allowed" }, false);
-        }
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith("image/");
+    if (isPhoto) {
+      next(null, true);
+    } else {
+      next({ message: "That file type isn't allowed" }, false);
     }
+  }
 };
 
 exports.homePage = (req, res) => {
-    res.render("index");
+  res.render("index");
 };
 
 exports.addStore = (req, res) => {
-    res.render("editStore", { title: "Add Store" });
+  res.render("editStore", { title: "Add Store" });
 };
 
 exports.upload = multer(multerOptions).single("photo");
 exports.resize = async (req, res, next) => {
-    //check if there is no new file to resize
-    if (!req.file) {
-        next(); // skip to the next middleware
-        return;
-    }
-    const extension = req.file.mimetype.split("/")[1];
-    req.body.photo = `${uuid.v4()}.${extension}`;
-    //now we resize
-    const photo = await jimp.read(req.file.buffer);
-    await photo.resize(800, jimp.AUTO);
-    await photo.write(`./public/uploads/${req.body.photo}`);
-    // once we have written the photo to our filesystem, keep going!
-    next();
+  //check if there is no new file to resize
+  if (!req.file) {
+    next(); // skip to the next middleware
+    return;
+  }
+  const extension = req.file.mimetype.split("/")[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+  //now we resize
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  // once we have written the photo to our filesystem, keep going!
+  next();
 };
 
 exports.createStore = async (req, res) => {
-    req.body.author = req.user._id;
-    const store = await new Store(req.body).save();
-    req.flash(
-        "success",
-        `Successfully Created ${store.name}. Care to leave a review?`
-    );
-    res.redirect(`/store/${store.slug}`);
+  req.body.author = req.user._id;
+  const store = await new Store(req.body).save();
+  req.flash(
+    "success",
+    `Successfully Created ${store.name}. Care to leave a review?`
+  );
+  res.redirect(`/store/${store.slug}`);
 };
 
 exports.getStores = async (req, res) => {
-    // 1. Query the database for a list of all stores
-    const stores = await Store.find();
-    res.render("stores", { title: "Stores", stores });
+  // 1. Query the database for a list of all stores
+  const stores = await Store.find();
+  res.render("stores", { title: "Stores", stores });
 };
 
 const confirmOwner = (store, user) => {
-    if (!store.author.equals(user._id)) {
-        throw Error("You must ow a store in order to edit it!!");
-    }
+  if (!store.author.equals(user._id)) {
+    throw Error("You must ow a store in order to edit it!!");
+  }
 };
 
 exports.editStore = async (req, res) => {
-    // 1. Find the store given ID
-    const store = await Store.findOne({ _id: req.params.id });
-    // res.json(store);
-    // 2. confirm they are the owner of the store
-    confirmOwner(store, req.user);
-    // 3. Render out the edit form so the user can update their store
-    res.render("editStore", { title: `Edit ${store.name}`, store });
+  // 1. Find the store given ID
+  const store = await Store.findOne({ _id: req.params.id });
+  // res.json(store);
+  // 2. confirm they are the owner of the store
+  confirmOwner(store, req.user);
+  // 3. Render out the edit form so the user can update their store
+  res.render("editStore", { title: `Edit ${store.name}`, store });
 };
 
 exports.updateStore = async (req, res) => {
-    //set the location data to be a point
-    req.body.location.type = "Point";
-    //find and update the store
-    const store = await Store.findOneAndUpdate(
-        { _id: req.params.id },
-        req.body,
-        {
-            new: true, // return the new store instead of the old one
-            runValidators: true
-        }
-    ).exec();
+  //set the location data to be a point
+  req.body.location.type = "Point";
+  //find and update the store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new store instead of the old one
+    runValidators: true
+  }).exec();
 
-    req.flash(
-        "success",
-        `Successfully updated <strong>${
-            store.name
-        }</strong>. <a href="/stores/${store.slug}">View Store -< </a>`
-    );
-    res.redirect(`/stores/${store._id}/edit`);
+  req.flash(
+    "success",
+    `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${
+      store.slug
+    }">View Store -< </a>`
+  );
+  res.redirect(`/stores/${store._id}/edit`);
 
-    // Redirect them the store and tell them  worked
+  // Redirect them the store and tell them  worked
 };
 
 exports.getStoreBySlug = async (req, res, next) => {
-    const store = await Store.findOne({ slug: req.params.slug }).populate(
-        "author"
-    );
-    if (!store) return next();
-    res.render("store", { store, title: store.name });
+  const store = await Store.findOne({ slug: req.params.slug }).populate(
+    "author reviews"
+  );
+  if (!store) return next();
+  res.render("store", { store, title: store.name });
 };
 
 exports.getStoresByTag = async (req, res) => {
-    const tag = req.params.tag;
-    const tagQuery = tag || { $exists: true };
-    const tagsPromise = Store.getTagsList();
-    const storePromise = Store.find({ tags: tagQuery });
-    const [tags, stores] = await Promise.all([tagsPromise, storePromise]);
+  const tag = req.params.tag;
+  const tagQuery = tag || { $exists: true };
+  const tagsPromise = Store.getTagsList();
+  const storePromise = Store.find({ tags: tagQuery });
+  const [tags, stores] = await Promise.all([tagsPromise, storePromise]);
 
-    res.render("tag", { tags, title: "Tags", tag, stores });
+  res.render("tag", { tags, title: "Tags", tag, stores });
 };
 
 exports.searchStores = async (req, res) => {
-    const stores = await Store.find(
-        //first find stores that match
-        {
-            $text: {
-                $search: req.query.q
-            }
-        },
-        {
-            score: { $meta: "textScore" }
-        }
-        //the sort them
-    )
-        .sort({
-            score: { $meta: "textScore" }
-        })
-        // limit to only 5 results
-        .limit(5);
-    res.json(stores);
+  const stores = await Store.find(
+    //first find stores that match
+    {
+      $text: {
+        $search: req.query.q
+      }
+    },
+    {
+      score: { $meta: "textScore" }
+    }
+    //the sort them
+  )
+    .sort({
+      score: { $meta: "textScore" }
+    })
+    // limit to only 5 results
+    .limit(5);
+  res.json(stores);
 };
 
 exports.mapStores = async (req, res) => {
-    const coordinates = [req.query.lng, req.query.lat].map(parseFloat);
-    const q = {
-        location: {
-            $near: {
-                $geometry: {
-                    type: "Point",
-                    coordinates: coordinates
-                },
-                $maxDistance: 10000 // 10km
-            }
-        }
-    };
+  const coordinates = [req.query.lng, req.query.lat].map(parseFloat);
+  const q = {
+    location: {
+      $near: {
+        $geometry: {
+          type: "Point",
+          coordinates: coordinates
+        },
+        $maxDistance: 10000 // 10km
+      }
+    }
+  };
 
-    const stores = await Store.find(q)
-        .select("slug name description location photo")
-        .limit(10);
-    res.json(stores);
+  const stores = await Store.find(q)
+    .select("slug name description location photo")
+    .limit(10);
+  res.json(stores);
 };
 
 exports.mapPage = (req, res) => {
-    res.render("map", { title: "Map" });
+  res.render("map", { title: "Map" });
 };
 
 exports.heartStore = async (req, res) => {
-    const hearts = req.user.hearts.map(obj => obj.toString());
-    const operator = hearts.includes(req.params.id) ? "$pull" : "$addToSet";
-    const user = await User.findByIdAndUpdate(
-        req.user._id,
-        {
-            [operator]: { hearts: req.params.id }
-        },
-        { new: true }
-    );
-    res.json(user);
+  const hearts = req.user.hearts.map(obj => obj.toString());
+  const operator = hearts.includes(req.params.id) ? "$pull" : "$addToSet";
+  const user = await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      [operator]: { hearts: req.params.id }
+    },
+    { new: true }
+  );
+  res.json(user);
 };
 
 exports.getHearts = async (req, res) => {
-    const stores = await Store.find({
-        _id: { $in: req.user.hearts }
-    });
-    res.render("stores", { title: "Hearted Stores", stores: stores });
+  const stores = await Store.find({
+    _id: { $in: req.user.hearts }
+  });
+  res.render("stores", { title: "Hearted Stores", stores: stores });
+};
+
+exports.getTopStores = async (req, res) => {
+  const stores = await Store.getTopStores();
+
+  res.render("topStores", { stores, title: "⭐ Top Stores!" });
 };
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const storeController = require("../controllers/storeController");
 const userController = require("../controllers/userController");
 const authController = require("../controllers/authController");
+const reviewController = require("../controllers/reviewController");
 const { catchErrors } = require("../handlers/errorHandlers");
 
 router.get("/", catchErrors(storeController.getStores));
@@ -10,17 +11,17 @@ router.get("/stores", catchErrors(storeController.getStores));
 router.get("/add", authController.isLoggedIn, storeController.addStore);
 
 router.post(
-    "/add",
-    storeController.upload,
-    catchErrors(storeController.resize),
-    catchErrors(storeController.createStore)
+  "/add",
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.createStore)
 );
 
 router.post(
-    "/add/:id",
-    storeController.upload,
-    catchErrors(storeController.resize),
-    catchErrors(storeController.updateStore)
+  "/add/:id",
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.updateStore)
 );
 
 router.get("/stores/:id/edit", catchErrors(storeController.editStore));
@@ -37,10 +38,10 @@ router.get("/register", userController.registerForm);
 // 2. register the user
 // 3. we need to log them in
 router.post(
-    "/register",
-    userController.validateRegister,
-    userController.register,
-    authController.login
+  "/register",
+  userController.validateRegister,
+  userController.register,
+  authController.login
 );
 
 router.get("/logout", authController.logout);
@@ -50,17 +51,25 @@ router.post("/account", catchErrors(userController.updateAccount));
 router.post("/account/forgot", catchErrors(authController.forgot));
 router.get("/account/reset/:token", catchErrors(authController.reset));
 router.post(
-    "/account/reset/:token",
-    authController.confirmedPasswords,
-    catchErrors(authController.update)
+  "/account/reset/:token",
+  authController.confirmedPasswords,
+  catchErrors(authController.update)
 );
 
 router.get("/map", storeController.mapPage);
 router.get(
-    "/hearts",
-    authController.isLoggedIn,
-    catchErrors(storeController.getHearts)
+  "/hearts",
+  authController.isLoggedIn,
+  catchErrors(storeController.getHearts)
 );
+
+router.post(
+  "/reviews/:id",
+  authController.isLoggedIn,
+  catchErrors(reviewController.addReview)
+);
+
+router.get("/top", catchErrors(storeController.getTopStores));
 
 /*
     API
